4b7c3e33d065de08beb5f46c82a8be01313aca7e
ionutfpop <48431816+ionutfpop@users.noreply.github.com>
2024-10-15 13:12:37 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,85 +1,64 @@
 import csv
-from collections import Counter
 
-
-# Functie pentru citirea cuvintelor din fisierul csv
-# Am utilizat codificare UTF-8 deoarece foloseste pentru codificarea caracterelor un octet (caracterele ASCII) pana la 4 octeti.
-# Diacriticele romanesti sunt codificate in UTF-8 pe 2 sau 3 octeti.
 def citeste_fisier(doccsv):
     pereche_cuvinte = []
     with open(doccsv, newline='', encoding='utf-8') as fisiercsv:
         citeste_cuvant = csv.reader(fisiercsv, delimiter=';')
         for linie in citeste_cuvant:
             if len(linie) == 3:
-                pereche_cuvinte.append((linie[1].upper(), linie[2].upper()))  # sir partial si cuvant complet
+                pereche_cuvinte.append((linie[1].upper(), linie[2].upper()))
     return pereche_cuvinte
 
-
-# Functie care calculeaza frecventa literelor din lista de cuvinte ramase
-def frecventa_litere(cuvinte):
-    contor_litere = Counter()
-    for cuvant in cuvinte:
-        litere_unice = set(cuvant)  # Calculam frecventa literelor unice din fiecare cuvant
-        contor_litere.update(litere_unice)
-    return contor_litere
-
-
-# Functie care ghiceste automat literele si returneaza numarul de incercari
-def joc_automat_hangman(cuvant_partial, cuvant_complet, max_greseli=7):
-    gresit = 0
+def joc_automat_hangman(cuvant_partial, cuvant_complet):
     utilizate = []
-    ghicit_partial = list(cuvant_partial)  # Cuvant ghicit partial
+    ghicit_partial = list(cuvant_partial)
     incercari = 0
 
-    while gresit < max_greseli and ''.join(ghicit_partial) != cuvant_complet:
-        # Calculeaza frecventa literelor din cuvantul complet care nu au fost inca ghicite
-        litere_ramase = [l for l in cuvant_complet if l not in utilizate]
+    while ''.join(ghicit_partial) != cuvant_complet:
+        alfabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZĂÂÎȘȚ'
+        litere_ramase = []
+
+        for litera_alfabet in alfabet:
+            if litera_alfabet in cuvant_complet and litera_alfabet not in utilizate:
+                litere_ramase.append(litera_alfabet) 
+
         if not litere_ramase:
             break
-        # Ghicim litera cu cea mai mare frecventa
-        max_frecventa_litera = frecventa_litere([litere_ramase])
-        ghicit = max_frecventa_litera.most_common(1)[0][0]
 
-        utilizate.append(ghicit)
+        litera_ghicita = litere_ramase[0]
+        utilizate.append(litera_ghicita)
         incercari += 1
 
-        if ghicit in cuvant_complet:
-            # Inlocuim '*' cu litera ghicita corect
+        if litera_ghicita in cuvant_complet:
             for i in range(len(cuvant_complet)):
-                if cuvant_complet[i] == ghicit:
-                    ghicit_partial[i] = ghicit
+                if cuvant_complet[i] == litera_ghicita:
+                    ghicit_partial[i] = litera_ghicita
         else:
-            gresit += 1
+            None 
 
     return incercari, (''.join(ghicit_partial) == cuvant_complet)
 
-
-# Functia principala care ruleaza Hangman pentru toate cuvintele din CSV si returneaza numarul total de incercari
 def rulare_hangman_csv(fisier):
     pereche_cuvinte = citeste_fisier(fisier)
     total_incercari = 0
     total_cuvinte_ghicite = 0
 
     for cuvant_partial, cuvant_complet in pereche_cuvinte:
-        incercari, ghicit = joc_automat_hangman(cuvant_partial, cuvant_complet)
+        incercari, cuvant_ghicit = joc_automat_hangman(cuvant_partial, cuvant_complet)
         total_incercari += incercari
-        if ghicit:
+        if cuvant_ghicit:
             total_cuvinte_ghicite += 1
-            print(f"Cuvant ghicit: {cuvant_complet} | Incercari: {incercari}")
-        else:
-            print(f"Nu a fost ghicit: {cuvant_complet} | Incercari: {incercari}")
         if total_incercari > 1200:
-            break  # Oprim daca depasim 1200 de incercari
+            break  
 
     return total_incercari, total_cuvinte_ghicite, len(pereche_cuvinte)
 
-
-# Apelam functia pentru fisierul CSV dat si afisam totalul incercarilor si numarul de cuvinte ghicite
 total_incercari, total_cuvinte_ghicite, total_cuvinte = rulare_hangman_csv('cuvinte_de_verificat1.csv')
 
 if total_incercari <= 1200:
-    print(f"Numarul total de incercari: {total_incercari}")
-    print(f"Numarul total de cuvinte ghicite: {total_cuvinte_ghicite} din {total_cuvinte}")
+    print(f"Numărul total de încercări: {total_incercari}")
+    print(f"Numărul de cuvinte ghicite: {total_cuvinte_ghicite} din {total_cuvinte}")
 else:
-    print(f"Numarul total de incercari depaseste 1200. Total incercari: {total_cuvinte}")
-    print(f"Numarul total de cuvinte ghicite: {total_cuvinte_ghicite} din {total_cuvinte}")
+    print(f"Numărul total de încercări depășește 1200. Total încercări: {total_incercari}")
+    print(f"Numărul total de cuvinte ghicite: {total_cuvinte_ghicite} din {total_cuvinte}")
+
